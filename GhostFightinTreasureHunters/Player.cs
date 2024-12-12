ca95cb754d46a27c24c3575f3c4e1ff368577b5e
JordyLahaye <jja.lahaye@gmail.com>
2024-12-12 16:40:33 +0100

    push

--- GhostFightinTreasureHunters/Player.cs
+++ GhostFightinTreasureHunters/Player.cs
@@ -8,15 +8,10 @@ namespace GhostFightinTreasureHunters
 {
     public class Player : Pawn
     {
-        private List<Int32> DieMoveIndex = new List<Int32>() { 1, 2, 3, 4, 5, 6 };
-        private List<String> DieAttackIndex = new List<String>() { "ghost", "ghost", "ghost", "hauntingmarker", "hauntingmarker", "hauntingmarker" };
-        //Of 3 en 3 is 1 op 1 kans op een of het ander dus : random 0 tot 1, if 1 dan ghost if 0 dan haunting, zelfde voor moveindex, random next(0,6)?
-        /*Random random = new Random();
-        int randint = random.Next(0, 2);
-        Console.WriteLine(randint);*/
-
+        
         public string Name { get; set; }
         public string ColorHunter { get; set; }
+        public bool HasJewel { get; set; }
         public Player(string name, string colorHunter) //CreatePlayer
         {
             Name = name;
