210da637e76912a8ba5c4a4ca0a90a9e0f754c8e
JordyLahaye <jja.lahaye@gmail.com>
2025-01-07 23:11:08 +0100

    ccc

--- GhostFightinTreasureHunters/CardDeck.cs
+++ GhostFightinTreasureHunters/CardDeck.cs
@@ -11,6 +11,7 @@ namespace GhostFightinTreasureHunters
 
         public List<string> ListOfCards { get; set; } = new List<string>();
         public List<string> RemainingCards { get; set; } = new List<string>();
+        private int gameId { get; set; }
 
         public void Shuffle()
         {
